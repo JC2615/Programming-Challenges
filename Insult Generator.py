f48b38c891f0444ca6e104d8587de23588b5cd4d
Josh Curry <joshuacurry2615@gmail.com>
2018-09-16 13:27:54 -0400

    Added user input and replaced lists with dictionaries.

--- Insult Generator.py
+++ Insult Generator.py
@@ -1,24 +1,30 @@
-import random
+from random import *
 
-input("Welcome to Insult Generator v1.0. Press enter to generate an insult. ")
+part1 = {1: "Shut up, you ", 2:"Sit down, you ", 3: "You're a ", 4: "This is the best way I could describe you: a "}
 
-part1 = ["Shut up, you ", "Sit down, you ", "You're a ", "This is the best way I could describe you: a "]
+part2 = {1: "stupid ", 2: "not so pretty ", 3: "rotten ", 4: "sickening ", 5: "wonderful ", 6: "dimwitted ", 7: "maniacal ",
+         8: "withering ", 9: "hollow shell of a ", 10: "dog hating ", 11: "global warming loving "}
 
-part2 = ["stupid ", "not so pretty ", "rotten ", "sickening ", "wonderful ", "dimwitted ", "maniacal ",
-         "withering ", "hollow shell of a ", "dog hating ", "global warming loving "]
-
-part3 = ["pig.", "rat.", "washboard.", "individual.",
-         "capitalist.", "Nazi.", "socialist.", "monster.", "dictator.", "waste of space."]
+part3 = {1: "pig.", 2: "rat.", 3: "washboard.", 4: "individual.",
+         5: "capitalist.", 6: "Nazi.", 7: "socialist.", 8: "monster.", 9: "dictator.", 10: "waste of space."}
 
 while True:
-    print("\n" + random.choice(part1) + random.choice(part2) + random.choice(part3))
 
+    numGen = int(input("Welcome to Insult Generator v1.0. Enter the amount of insults you'd like to generate: "))
+
+    for i in range(numGen):
+        p1 = randint(1,4)
+        p2 = randint(1,11)
+        p3 = randint(1,10)
+
+        print("\n" + part1[p1] + part2[p2] + part3[p3])
+    
     play_again = input("\nWanna play again? (Y/N): ").lower()
     if play_again == "y":
         continue
     elif play_again == "n":
         break
     else:
-        print("'%s' is an invalid answer. Since you lack the ability to follow instructions, Insult Generator will shut "
+        print("'%s' is an invalid answer. Since you lack the ability to follow instructions, Insult Generator v2.0 will shut "
               "down." % play_again)
         break
